907da2cbbea8bade10c780bae1d00f4342204333
Tony Murray <murraytony@gmail.com>
2017-08-09 00:06:25 -0500

    Make composer_wrapper.php respect http_proxy or HTTP_PROXY Drop https on the url to simplify things, we already do a SHA384 check...

--- scripts/composer_wrapper.php
+++ scripts/composer_wrapper.php
@@ -35,6 +35,20 @@ if (!empty($path_exec)) {
 } elseif (is_file($install_dir . '/composer.phar')) {
     $exec = 'php ' . $install_dir . '/composer.phar';
 } else {
+    // Set up proxy if needed
+    $proxy = getenv("http_proxy") ?: getenv("HTTP_PROXY");
+    var_dump($proxy);
+    if ($proxy) {
+        $stream_default_opts = array(
+            'http' => array(
+                'proxy' => str_replace('http://', 'tcp://', $proxy),
+                'request_fulluri' => true,
+            )
+        );
+
+        stream_context_set_default($stream_default_opts);
+    }
+
     // Download composer.phar (code from the composer web site)
     @copy('https://getcomposer.org/installer', 'composer-setup.php');
     if (@hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') {
